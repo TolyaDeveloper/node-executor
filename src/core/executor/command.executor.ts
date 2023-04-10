afd5d198f5de44780a9b00b36322da3d7c80fad7
Anatolii <49586997+TolyaDeveloper@users.noreply.github.com>
2023-04-10 10:59:53 +0300

    template method pattern for executor (#11)

---
+++ src/core/executor/command.executor.ts
@@ -0,0 +1,26 @@
+import { ChildProcessWithoutNullStreams } from 'child_process'
+
+import { ILoggerService } from '~/logger/logger.service.interface'
+import { ICommandExecutor } from './command.executor.interface'
+
+export abstract class CommandExecutor<Input> {
+  constructor(private logger: ILoggerService) {}
+
+  public async execute() {
+    const input = await this.prompt()
+    const command = this.build(input)
+    const stream = this.spawn(command)
+
+    this.processStream(stream, this.logger)
+  }
+
+  protected abstract prompt(): Promise<Input>
+  protected abstract build(input: Input): ICommandExecutor
+  protected abstract spawn(
+    command: ICommandExecutor
+  ): ChildProcessWithoutNullStreams
+  protected abstract processStream(
+    stream: ChildProcessWithoutNullStreams,
+    logger: ILoggerService
+  ): void
+}
